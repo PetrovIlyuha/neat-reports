90215e99001cf93e623318ba56703973ee2037aa
Ilya Petrov <dnamix1@gmail.com>
2020-03-06 11:51:24 +0300

    authentication routes/ sign-in page layout added

--- src/pages/routes.js
+++ src/pages/routes.js
@@ -2,11 +2,14 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import GlobalFeed from "./GlobalFeed";
 import SingleArticle from "./SingleArticle";
+import Authentication from "./Authentication";
 
 export default () => {
   return (
     <Switch>
       <Route exact path="/" component={GlobalFeed} />
+      <Route path="/login" component={Authentication} />
+      <Route path="/register" component={Authentication} />
       <Route path="/articles/:slug" component={SingleArticle} />
     </Switch>
   );
