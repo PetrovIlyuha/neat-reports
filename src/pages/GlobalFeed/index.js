c5f545209f412c06d867acfabc7fe94a465f55b9
Ilya Petrov <dnamix1@gmail.com>
2020-03-08 12:23:00 +0300

    posts feed layout implemented

--- src/pages/GlobalFeed/index.js
+++ src/pages/GlobalFeed/index.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import useFetch from "../../hooks/useFetch";
+import Feed from "../../components/Feed";
 
 const GlobalFeed = () => {
-  return <div>Global Feed</div>;
+  const apiUrl = "/articles?limit=10&offset=0";
+  const [{ isLoading, response, error }, doFetchData] = useFetch(apiUrl);
+
+  useEffect(() => {
+    doFetchData();
+  }, [doFetchData]);
+  return (
+    <div className="home-page">
+      <div className="banner">
+        <div className="container">
+          <h1>Neat Reports</h1>
+          <p>All Cool Minds Have Gathered Here</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default GlobalFeed;
