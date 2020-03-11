e7dbc80adb67301fffa05c1f488883324f5ab1ba
Ilya Petrov <dnamix1@gmail.com>
2020-03-11 12:42:21 +0300

    article form final version properly handling state variables and proper submission to the back-end

--- src/components/ArticleForm.js
+++ src/components/ArticleForm.js
@@ -1,21 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
+import BackendErrorMessage from "./BackendErrors/BackendErrorMessage";
 const ArticleForm = ({ onSubmit, errors, initialValues }) => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [description, setDescription] = useState("");
   const [tagList, setTagList] = useState("");
-
   const handleSubmit = event => {
     event.preventDefault();
-    onSubmit({ foo: "Foo" });
+    const article = {
+      title,
+      description,
+      body,
+      tagList
+    };
+    onSubmit(article);
   };
+
+  useEffect(() => {
+    if (!initialValues) {
+      return;
+    }
+    setTitle(initialValues.title);
+    setDescription(initialValues.description);
+    setBody(initialValues.body);
+    setTagList(initialValues.tagList.join(" "));
+  }, [initialValues]);
+
   return (
     <div className="editor-page">
       <div className="container page">
         <div className="row">
           <div className="col-md-10 offset-md-1 col-xs-12">
-            BackendErrorMessages
+            {errors && <BackendErrorMessage backendErrors={errors} />}
             <form onSubmit={handleSubmit}>
               <fieldset>
                 <fieldset className="form-group">
@@ -23,6 +40,8 @@ const ArticleForm = ({ onSubmit, errors, initialValues }) => {
                     type="text"
                     className="form-control form-control-lg"
                     placeholder="Article Title"
+                    value={title}
+                    onChange={e => setTitle(e.target.value)}
                   />
                 </fieldset>
 
@@ -31,6 +50,8 @@ const ArticleForm = ({ onSubmit, errors, initialValues }) => {
                     type="text"
                     className="form-control form-control-lg"
                     placeholder="Article Description in general..."
+                    value={description}
+                    onChange={e => setDescription(e.target.value)}
                   />
                 </fieldset>
 
@@ -39,6 +60,8 @@ const ArticleForm = ({ onSubmit, errors, initialValues }) => {
                     className="form-control"
                     placeholder="Here you can write your article (in markdown)"
                     rows="10"
+                    value={body}
+                    onChange={e => setBody(e.target.value)}
                   ></textarea>
                 </fieldset>
 
@@ -47,6 +70,8 @@ const ArticleForm = ({ onSubmit, errors, initialValues }) => {
                     type="text"
                     className="form-control form-control-lg"
                     placeholder="Enter descriptive tags..."
+                    value={tagList}
+                    onChange={e => setTagList(e.target.value)}
                   />
                 </fieldset>
 
--- src/pages/CreateArticle/index.js
+++ src/pages/CreateArticle/index.js
@@ -1,16 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Redirect } from "react-router-dom";
 import ArticleForm from "../../components/ArticleForm";
+import useFetch from "../../hooks/useFetch";
 
 const CreateArticle = () => {
-  const errors = {};
-  const initialValues = {};
-  const handleSubmit = data => {
-    console.log("handlesubmit", data);
+  const apiUrl = "/articles";
+  const [{ isLoading, response, error }, doFetchData] = useFetch(apiUrl);
+  const initialValues = {
+    title: "",
+    description: "",
+    body: "",
+    tagList: []
   };
+  const [isSuccessfulSubmit, setIsSuccessfulSubmit] = useState(false);
+  const handleSubmit = article => {
+    doFetchData({
+      method: "post",
+      data: {
+        article
+      }
+    });
+  };
+  useEffect(() => {
+    if (!response) {
+      return;
+    }
+    setIsSuccessfulSubmit(true);
+  }, [response]);
+
+  if (isSuccessfulSubmit) {
+    return <Redirect to={`/articles/${response.article.slug}`} />;
+  }
   return (
     <div>
       <ArticleForm
-        errors={errors}
+        errors={(error && error.errors) || {}}
         initialValues={initialValues}
         onSubmit={handleSubmit}
       />
