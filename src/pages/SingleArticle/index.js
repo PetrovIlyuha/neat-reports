43e7a658962022cfcd8526b83dadc2fbaad09852
Ilya Petrov <dnamix1@gmail.com>
2020-03-13 16:31:56 +0300

    deleting existing article only by user that have created it implemented

--- src/pages/SingleArticle/index.js
+++ src/pages/SingleArticle/index.js
@@ -1,19 +1,37 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
+
 import Loading from "../../components/Loading";
 import ErrorMessage from "../../components/ErrorMessage";
 import TagList from "../../components/TagList";
+
 import useFetch from "../../hooks/useFetch";
+import { CurrentUserContext } from "../../contexts/currentUser";
+
+import { AiTwotoneEdit } from "react-icons/ai";
+import { FaTrashAlt } from "react-icons/fa";
 
 const SingleArticle = props => {
   const slug = props.match.params.slug;
   const apiUrl = `/articles/${slug}`;
   const [{ isLoading, response, error }, doFetchData] = useFetch(apiUrl);
+  const [currentUserState] = useContext(CurrentUserContext);
+
+  const isAuthor = () => {
+    if (!response || !currentUserState.isLoggedIn) {
+      return false;
+    }
+    return (
+      response.article.author.username === currentUserState.currentUser.username
+    );
+  };
 
   useEffect(() => {
     doFetchData();
   }, [doFetchData]);
-
+  const deleteArticle = () => {
+    console.log("delete article");
+  };
   return (
     <div className="article-page">
       <div className="banner">
@@ -30,6 +48,25 @@ const SingleArticle = props => {
                 </Link>
                 <span className="date">{response.article.createdAt}</span>
               </div>
+              {isAuthor() && (
+                <span>
+                  <Link
+                    className="btn btn-outline-secondary btn-sm"
+                    to={`/articles/${response.article.slug}/edit`}
+                  >
+                    <AiTwotoneEdit />
+                    Edit Article
+                  </Link>
+                  <button
+                    onClick={deleteArticle}
+                    className="btn btn-outline-danger btn-sm"
+                    style={{ marginLeft: "10px" }}
+                  >
+                    <FaTrashAlt />
+                    Remove Post from Feed
+                  </button>
+                </span>
+              )}
             </div>
           </div>
         )}
